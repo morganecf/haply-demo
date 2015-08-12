e1ff867da842adcfe61eda2ad94ff6d3c935000a
Morgane Ciot <morganeciot@gmail.com>
2015-08-12 16:03:09 -0400

    cleaned up main dir

--- setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup 
-from setuptools.extension import Extension
-from Cython.Build import cythonize 
-
-setup(ext_modules = cythonize(Extension('ball', sources=['pyball.pyx', 'ball.cpp'], language='c++')))
